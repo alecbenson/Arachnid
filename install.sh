574b079967d3a74df6ae050d991f944c59afce66
alec12094 <abenson@wpi.edu>
2015-04-24 03:53:19 -0400

    Updated install file

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-echo Installing scapy..
-sudo apt-get install pip-python
+echo Installing scapy & netfilterqueue
+sudo apt-get install python-pip build-essential python-dev libnetfilter-queue-dev
 sudo pip install scapy
+sudo pip install netfilterqueue
 echo fuck yeh
