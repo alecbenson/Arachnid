9a94d6ed447b5886248c31a53407df745cd73f6b
Alec Benson <abenson@wpi.edu>
2015-04-26 12:57:51 -0400

    Added deploy file and moved setup scripts to the script folder

---
+++ setup scripts/deploy.sh
@@ -0,0 +1,17 @@
+
+
+cd Desktop
+echo "Cloning repo if it doesn't exist."
+git clone https://github.com/zoccihedron/Arachnid.git
+cd Arachnid
+git pull
+
+
+sudo scp -r ~/Desktop/Arachnid cs4404@10.4.4.1:~/
+sudo scp -r ~/Desktop/Arachnid cs4404@10.4.4.2:~/
+sudo scp -r ~/Desktop/Arachnid cs4404@10.4.4.3:~/
+sudo scp -r ~/Desktop/Arachnid cs4404@10.4.4.4:~/
+sudo scp -r ~/Desktop/Arachnid cs4404@10.4.4.5:~/
+sudo scp -r ~/Desktop/Arachnid cs4404@10.4.4.6:~/
+sudo scp -r ~/Desktop/Arachnid cs4404@10.4.4.7:~/
+sudo scp -r ~/Desktop/Arachnid cs4404@10.4.4.8:~/
