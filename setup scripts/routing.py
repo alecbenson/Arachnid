572c0544751f057d2a16a2861fe43329924835cf
Alec Benson <abenson@wpi.edu>
2015-04-26 14:57:17 -0400

    Fixed routing table setup script

--- setup scripts/routing.py
+++ setup scripts/routing.py
@@ -7,7 +7,7 @@ node_id = int( input("Enter node #: ") )
 nodes = range(node_id, vm_count)
 
 for index in nodes:
-	call(["sudo", "route", "add", "-host", "10.4.4.{0}".format( str(index + 1) ), "gw", "10.4.4.{0}".format( nodes[1] )] )
+	call(["sudo", "route", "add", "-host", "10.4.4.{0}".format( str(index + 1) ), "gw", "10.4.4.{0}".format( node_id + 1 )] )
 
 nodes = range(1, node_id)
 for index in nodes[:node_id-1]:
