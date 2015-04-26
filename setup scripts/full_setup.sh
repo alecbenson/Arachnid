39617498f27856c7277fe3e68fc88bcf7a8f0ad0
Alec Benson <abenson@wpi.edu>
2015-04-26 15:53:38 -0400

    Added do-it-all setup script because I'm tired of reinstalling everything whenever a vm goes down

---
+++ setup scripts/full_setup.sh
@@ -0,0 +1,16 @@
+echo "Starting setup..."
+sudo apt-get install python-dev python-setuptools libnetfilter-queue-dev git
+cd ../deps
+
+cd dpkt-1.8
+sudo python setup.py install
+cd ../netifaces-0.10.4
+sudo python setup.py install
+cd ../pycrypto-2.6.1
+sudo python setup.py install
+cd ../python-netfilterqueue
+sudo python setup.py install
+
+cd ../../setup\ scripts
+sudo sh nfqueue-iptable.sh
+sudo python routing.py
